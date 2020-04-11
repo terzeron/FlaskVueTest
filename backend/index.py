4079789f13b0338745df254e1b76c82b767ad03e
Youngil Cho <terzeron@gmail.com>
2020-04-11 09:54:38 +0900

    add book api

--- backend/index.py
+++ backend/index.py
@@ -3,16 +3,28 @@
 
 
 import uuid
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = "./dist/static", template_folder = "./dist")
 app.config.from_object(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route("/ping", methods=["GET"])
+
+@app.route('/', defaults={'path': ''})
+#@app.route('/<path:path>')
+def catch_all(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
+
+@app.route('/ping')
+def ping():
+    return render_template("index.html")
+
+@app.route("/healthcheck", methods=["GET"])
 def ping_pong():
     return jsonify("pong")
 
